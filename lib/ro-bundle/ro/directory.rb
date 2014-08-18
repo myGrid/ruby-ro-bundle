a1e389ca9440ce3df5dc093bc10025f05ed9a7ce
Robert Haines <rhaines@manchester.ac.uk>
2014-08-18 15:27:09 +0100

    Keep an internal manifest reference in RODir.
    Useful for manifest operations directly from RODir.

--- lib/ro-bundle/ro/directory.rb
+++ lib/ro-bundle/ro/directory.rb
@@ -20,7 +20,9 @@ module ROBundle
     # Create a new .ro managed directory entry with the specified manifest
     # file object.
     def initialize(manifest)
-      super(DIR_NAME, :required => true, :entries => manifest)
+      @manifest = manifest
+
+      super(DIR_NAME, :required => true, :entries => @manifest)
     end
   end
 
